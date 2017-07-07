9e15c51e0a90db9f4f9acd99fc5068657a1ad2fa
Philipp <philipp.boettcher@next-energy.de>
2017-07-07 08:42:46 +0200

    Added integrators.py and started to write runge kutta and euler solver.

---
+++ integrators/integrators.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import numpy as np
+
+def euler_method(ode, y, t_axis, dt):
+    """Take the ordinary differential equation and integrate it using the explicit euler method
+    
+    Parameters:
+    -----------
+    ode: Function that defines the ordinary differential equation.
+    y: current system state
+    t_axis: array like points of time that the ode is solved at
+    dt: stepwidth in time
+  
+    """
+
+
+
+
+def runge_kutta_4thorder(ode, y, ts):
+
+    def rk4(ode,y,ts,h=None):
+        i=0
+        th=
