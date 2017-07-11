21f239c4d8fab24915fe5b9744725c77ef00abf4
Philipp Böttcher <Philipp.Boettcher@next-energy.de>
2017-07-11 15:40:56 +0200

    Runge Kutta works for two examples and ODEs. Added test script to understand how it works. See solvers/ folder.

---
+++ ode_solvers/integrators_test.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python3
+
+import numpy as np
+import solvers_pcb
+
+import imp
+imp.reload(solvers_pcb)
+
+from matplotlib import pyplot as plt
+
+class test_solver:
+
+    def __init__(self):
+        self.h = 0.05
+        self.t_axis = np.arange(0, 5, self.h)
+        
+        self.gg = 9.81
+        self.beta = 1
+        self.mm = 85
+        
+        
+    def ode_decay(self, t, y):
+        """RHS of exponetial decay"""
+
+        dy_dt = y
+
+        return dy_dt
+    
+    def integrate_ode_decay(self):
+       
+        #Sovle the decay ode for y_0 = 1:
+        y_decay = np.array([1])
+        res_rk4 = solvers_pcb.integrate_runge_kutta_4thorder(self.ode_decay, self.t_axis, y_decay)
+        
+        #Analytical Solution to compare to:
+        acc_solution = np.exp(self.t_axis)
+        
+        
+        fig, ax_arr = plt.subplots(2,1, sharex=True)
+        fig.suptitle("$dx/dt = x $")
+        ax = ax_arr.flatten()
+
+        
+        ax[0].plot(self.t_axis, res_rk4, label="Runge-Kutta 4th order", marker = 'x', linestyle='', color='g')
+        ax[0].plot(self.t_axis, acc_solution, label="Analytical Solution",color='k')
+        ax[0].set_ylabel("$x$")
+        
+        ax[1].plot(self.t_axis, res_rk4[:,0] - acc_solution, label="Difference", marker = 'x', color='g')
+        ax[1].set_xlabel("$t$")
+        ax[1].set_ylabel("$\Delta x = x_{rk4}-x(t)$")
+        
+        
+
+        ax[0].legend()
+        plt.tight_layout()
+        plt.show()
+
+        plt.close('all')
+        fig.clear()
+        return res_rk4
+
+    def ode_fall_with_resistance(self, t, y):
+        """RHS of the differential equation that describes free fall with Stokes-Resistance:
+
+        y[0] = d/dt v(t) = -gg - (beta / mm) v(t)
+        y[1] = d/dt x(t) = v(t)
+
+        Parameter:
+        ----------
+        gg: gravitational acceleration
+        beta: coeffecient of stokes resist
+        mm: mass
+        """
+        dv_dt = - self.gg - (self.beta / self.mm ) * y[0]
+        dx_dt = y[0]
+
+        return np.asarray([dv_dt, dx_dt])
+
+    def integrate_ode_fall_ariresistance(self):
+
+        
+        y_fall = np.array([0, 0])
+        
+        v_0 = y_fall[0]
+        x_0 = y_fall[1]
+        
+        res_rk4 = solvers_pcb.integrate_runge_kutta_4thorder(self.ode_fall_with_resistance, self.t_axis, y_fall)        
+        res_euler = solvers_pcb.integrate_
+        
+        acc_solution = np.empty((len(self.t_axis), 2))
+
+        acc_solution[:,0] = ((-self.mm*self.gg)/self.beta)* ( 1- np.exp(-(self.beta/self.mm)*self.t_axis )) + v_0 * np.exp(-(self.beta/self.mm)*self.t_axis )
+        acc_solution[:,1] = ( v_0 + (self.mm * self.gg) / self.beta ) * ( self.mm / self.beta) * ( 1- np.exp(-(self.beta/self.mm) * self.t_axis )) - ( (self.mm*self.gg) / self.beta ) * self.t_axis + x_0
+        
+        
+        fig, ax_arr = plt.subplots(2,1)
+        ax = ax_arr.flatten()
+
+        ax[0].plot(self.t_axis, acc_solution[:,0], label="$v(t)$ analytical")
+        ax[0].plot(self.t_axis, res_rk4[:,0], label="Runge-Kutta 4th order $v(t)$", marker = "x", linestyle="")
+        ax[0].set_ylabel("v(t)")
+
+        ax[1].plot(self.t_axis, acc_solution[:,1], label="$x(t)$ analytical")
+        ax[1].plot(self.t_axis, res_rk4[:,1], label="Runge-Kutta 4th order $x(t)$", marker = "x", linestyle="")
+
+        ax[1].set_ylabel("$x(t)$")
+        ax[1].set_xlabel("$t$") 
+        
+
+        ax[0].legend()
+        plt.tight_layout()
+        plt.show()
+
+        plt.close('all')
+        fig.clear()
+        return res_rk4
+
+if __name__=="__main__":
+
+    test_cls = test_solver()
+
+    print("Example for the Runge-Kutta 4th for fall with air resistance")
+    
+    test_cls.integrate_ode_fall_ariresistance()
+    
+        
+
+
+
+
---
+++ ode_solvers/solvers_pcb.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+import numpy as np
+
+def integrate_euler_method(f, y, t_axis, dt):
+    """Take the ordinary differential equation and integrate it using the explicit euler method
+    
+    Parameters:
+    -----------
+    ode: Function that defines the ordinary differential equation.
+    y: current system state
+    t_axis: array like points of time that the ode is solved at
+    dt: stepwidth in time
+  
+    """
+
+    def next_euler_method(t_n, y_n, h):
+        y_next = y_n + h * f(t_n, y_n)
+
+        return y_n
+
+    def check_prerequisites_set_stepwidth():
+        #check equally spaced:
+        if np.diff(t_axis).std() >= 10**-6:
+            raise ValueError("t_axis is not equally spaced")
+        #Return stepwidth:
+
+        return np.diff(t_axis).mean()
+
+    h = check_prerequisites_set_stepwidth()
+
+    res_euler = []
+    y_current = y.copy()
+
+    for idx, tt in enumerate(t_axis):
+        
+        res_euler.append(y_current)
+        y_current = next_euler_method( tt, y_current, h)
+        
+    return np.asarray(res_euler)
+
+
+def integrate_runge_kutta_4thorder(f, t_axis, y,  h=None):
+    """Find the stepwise solution of the ode f using the 4th order Runge Kutta method. Sadly the formulas are taken from internet resources aka wikipedia...so be carefull
+
+    Parameters:
+    -----------
+    f: ordinary differential equation takes arguments f(t, y) = d/dt. Return value has to be an np.ndarray. Fix this ??
+    y: system state at this time
+    ts: time_axis. That has to be checked to be equally spaced
+
+    """
+    def next_runge_kutta_4th(t_n, y_n, h):
+        """Find the next value of rk4"""
+        k1 = f( t_n, y_n )
+        k2 = f( t_n + .5*h, y_n + .5 * h * k1)
+        k3 = f( t_n + .5*h, y_n + .5 * h * k2)
+        k4 = f( t_n + h, y_n + h * k3)
+
+        y_next = y_n + (1/6.) * h *( k1 + 2*k2 + 2*k3 + k4 )
+        #print( y_next)
+        return y_next
+
+
+    
+    def check_prerequisites_set_stepwidth():
+        #check equally spaced:
+        if np.diff(t_axis).std() >= 10**-6:
+            raise ValueError("t_axis is not equally spaced")
+        
+        #Return stepwidth:
+        return np.diff(t_axis).mean()
+
+    h = check_prerequisites_set_stepwidth()
+
+
+    res_rk4 = []
+    y_current = y.copy()
+
+    for idx, tt in enumerate(t_axis):
+        
+        res_rk4.append(y_current)
+        y_current = next_runge_kutta_4th( tt, y_current, h)
+        
+    return np.asarray(res_rk4)
