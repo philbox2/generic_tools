b1fb8a3d8c4e3f67aa645dfad3f60b30f489069c
Philipp Böttcher <pboettcher@itp.uni-bremen.de>
2017-07-17 22:32:09 +0200

    Changed to classes and modified the test accordingly.

--- ode_solvers/integrators_test.py
+++ ode_solvers/integrators_test.py
@@ -36,34 +36,45 @@ class test_solver:
        
         #Sovle the decay ode for y_0 = 1:
         y_decay = np.array([1])
-        res_rk4 = solvers_pcb.integrate_runge_kutta_4thorder(self.ode_decay, self.t_axis, y_decay)
+
+        r = solvers_pcb.integrate_runge_kutta_4thorder(self.ode_decay)
+        r.set_initial_values(y_decay, self.t_axis[0])
+
+        results = []
+        results.append(y_decay)
+        for idx, tt in enumerate(self.t_axis[:-1]):
+            results.append(r.integrate(tt, self.h)) 
         
+        res_rk4 = np.asarray(results)
+
+        
+        res_rk4_axis = r.integrate_along_taxis(self.t_axis, y_decay)
         #Analytical Solution to compare to:
         acc_solution = np.exp(self.t_axis)
         
         
         fig, ax_arr = plt.subplots(2,1, sharex=True)
+        fig = plt.gcf()
+        fig.canvas.set_window_title('Exponential Growth')
         fig.suptitle("$dx/dt = x $")
         ax = ax_arr.flatten()
 
         
         ax[0].plot(self.t_axis, res_rk4, label="Runge-Kutta 4th order", marker = 'x', linestyle='', color='g')
+        ax[0].plot(self.t_axis, res_rk4_axis, label="RK4th along t_axis", marker = '+', linestyle="", color='r')
         ax[0].plot(self.t_axis, acc_solution, label="Analytical Solution",color='k')
         ax[0].set_ylabel("$x$")
         
         ax[1].plot(self.t_axis, res_rk4[:,0] - acc_solution, label="Difference", marker = 'x', color='g')
+        ax[1].plot(self.t_axis, res_rk4_axis[:,0] - acc_solution, label="Difference along axis", marker = '+', color='r')
         ax[1].set_xlabel("$t$")
         ax[1].set_ylabel("$\Delta x = x_{rk4}-x(t)$")
         
         
 
-        ax[0].legend()
+        ax[0].legend(numpoints=1)
         plt.tight_layout()
-        plt.show()
-
-        plt.close('all')
-        fig.clear()
-        return res_rk4
+        return fig
 
     def ode_fall_with_resistance(self, t, y):
         """RHS of the differential equation that describes free fall with Stokes-Resistance:
@@ -90,8 +101,17 @@ class test_solver:
         v_0 = y_fall[0]
         x_0 = y_fall[1]
         
-        res_rk4 = solvers_pcb.integrate_runge_kutta_4thorder(self.ode_fall_with_resistance, self.t_axis, y_fall)        
+        #res_rk4 = solvers_pcb.integrate_runge_kutta_4thorder(self.ode_fall_with_resistance, self.t_axis, y_fall)        
         #res_euler = solvers_pcb.integrate_euler_method(self.ode_fall_with_resistance, self.t_axis, y_fall)
+
+        r = solvers_pcb.integrate_runge_kutta_4thorder(self.ode_fall_with_resistance)
+        r.set_initial_values(y_fall, self.t_axis[0])
+        results = []
+        results.append(y_fall)
+        for idx, tt in enumerate(self.t_axis[:-1]):
+            results.append(r.integrate(tt, self.h)) 
+        
+        res_rk4 = np.asarray(results)
         
         acc_solution = np.empty((len(self.t_axis), 2))
 
@@ -100,13 +120,15 @@ class test_solver:
         
         
         fig, ax_arr = plt.subplots(2,1)
+        fig = plt.gcf()
+        fig.canvas.set_window_title('Fall with Stokes Air-Resistance')
         ax = ax_arr.flatten()
 
         ax[0].plot(self.t_axis, acc_solution[:,0], label="$v(t)$ analytical")
-        ax[0].plot(self.t_axis, res_rk4[:,0], label="Runge-Kutta 4th order $v(t)$", marker = "x", linestyle="")
+        ax[0].plot(self.t_axis, res_rk4[:,0], label="Runge-Kutta 4th order", marker = "x", linestyle="")
         #ax[0].plot(self.t_axis, res_euler[:,0], label="Runge-Kutta 4th order $v(t)$", marker = "x", linestyle="")
         
-        ax[0].set_ylabel("v(t)")
+        ax[0].set_ylabel("$v(t)$")
 
         ax[1].plot(self.t_axis, acc_solution[:,1], label="$x(t)$ analytical")
         ax[1].plot(self.t_axis, res_rk4[:,1], label="Runge-Kutta 4th order $x(t)$", marker = "x", linestyle="")
@@ -116,13 +138,10 @@ class test_solver:
         ax[1].set_xlabel("$t$") 
         
 
-        ax[0].legend()
+        ax[0].legend(numpoints=1)
         plt.tight_layout()
-        plt.show()
-
-        plt.close('all')
-        fig.clear()
-        return res_rk4
+        
+        return fig
 
 if __name__=="__main__":
 
@@ -130,11 +149,14 @@ if __name__=="__main__":
 
     print("Example for exponential growth Runge-Kutta 4th")
 
-    test_cls.integrate_ode_decay()
+    fig1 = test_cls.integrate_ode_decay()
 
     print("Example for the Runge-Kutta 4th for fall with air resistance")
     
-    test_cls.integrate_ode_fall_ariresistance()
+    fig2 = test_cls.integrate_ode_fall_ariresistance()
+
+    fig1.show()
+    fig2.show()
     
         
 
--- ode_solvers/solvers_pcb.py
+++ ode_solvers/solvers_pcb.py
@@ -42,7 +42,7 @@ def integrate_euler_method(f, y, t_axis):
     return np.asarray(res_euler)
 
 
-def integrate_runge_kutta_4thorder(f, t_axis, y):
+class integrate_runge_kutta_4thorder:
     """Find the stepwise solution of the ode f using the 4th order Runge Kutta method. Sadly the formulas are taken from internet resources aka wikipedia...so be carefull
 
     Parameters:
@@ -52,39 +52,63 @@ def integrate_runge_kutta_4thorder(f, t_axis, y):
     ts: time_axis. That has to be checked to be equally spaced
 
     """
-    def next_runge_kutta_4th(t_n, y_n, h):
+
+    def __init__(self, ff):
+        self.ff = ff
+        self.y = None
+        self.tt = None
+
+    def set_initial_values(self, y0, t0):
+        self.y = y0
+        self.tt = t0
+        
+    def next_runge_kutta_4th(self, t_n, y_n, h):
         """Find the next value of rk4"""
-        k1 = f( t_n, y_n )
-        k2 = f( t_n + .5*h, y_n + .5 * h * k1)
-        k3 = f( t_n + .5*h, y_n + .5 * h * k2)
-        k4 = f( t_n + h, y_n + h * k3)
+        k1 = self.ff( t_n, y_n )
+        k2 = self.ff( t_n + .5*h, y_n + .5 * h * k1)
+        k3 = self.ff( t_n + .5*h, y_n + .5 * h * k2)
+        k4 = self.ff( t_n + h, y_n + h * k3)
 
         y_next = y_n + (1/6.) * h *( k1 + 2*k2 + 2*k3 + k4 )
-        #print( y_next)
+        
         return y_next
 
 
-    
-    def check_prerequisites_set_stepwidth():
-        #check equally spaced:
-        if np.diff(t_axis).std() >= 10**-6:
-            raise ValueError("t_axis is not equally spaced")
         
-        #Return stepwidth:
-        return np.diff(t_axis).mean()
-    def print_progress(x):
+
+    def integrate(self, tt, h):
+        if self.y is None or self.tt is None:
+            raise ValueError("Initialise class first with initial values.")
+        
+        y_current = self.next_runge_kutta_4th( tt, self.y, h)
+        self.y = y_current
+        
+        return y_current
+
+    def integrate_along_taxis(self, t_axis, y0, show_progress=False):
+
+        def print_progress(x):
             print("\rProgress: [{0:50s}] {1:.1f}%".format('#' * int(x * 50), x * 100), end="", flush=True)
             
-    h = check_prerequisites_set_stepwidth()
+        def check_prerequisites_set_stepwidth():
+            #check equally spaced:
+            if np.diff(t_axis).std() >= 10**-6:
+                raise ValueError("t_axis is not equally spaced")
+                #Return stepwidth:
 
+            return np.diff(t_axis).mean()
 
-    res_rk4 = []
-    y_current = y.copy()
-
-    tN = len(t_axis)
-    for idx, tt in enumerate(t_axis):
-        print_progress(idx/tN)
-        res_rk4.append(y_current)
-        y_current = next_runge_kutta_4th( tt, y_current, h)
+        hh = check_prerequisites_set_stepwidth()
         
-    return np.asarray(res_rk4)
+        res_rk4 = []
+
+        y_current = y0.copy()
+        
+        tN = len(t_axis)-1
+        for idx, tt in enumerate(t_axis):
+            if show_progress:
+                print_progress(idx/tN)
+            res_rk4.append(y_current)
+            y_current = self.next_runge_kutta_4th( tt, y_current, hh)
+
+        return np.asarray(res_rk4)
